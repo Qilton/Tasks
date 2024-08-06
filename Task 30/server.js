beaf0d1ecb3bd42a6382904bc94e768ce508c590
Qilton <swayambhalotia@gmail.com>
2024-08-06 23:47:15 +0530

    dsa

--- Task 30/server.js
+++ Task 30/server.js
@@ -1,11 +1,11 @@
-// server.js
+
 const http = require('http');
 const fs = require('fs');
 const path = require('path');
 
 const port = 3000;
 
-// Create an HTTP server
+
 const server = http.createServer((req, res) => {
   let filePath = '.' + req.url;
   if (filePath === './') filePath = './home.html';
@@ -18,17 +18,13 @@ const server = http.createServer((req, res) => {
 
   fs.readFile(filePath, (err, content) => {
     if (err) {
-      if (err.code === 'ENOENT') {
-        // Page Not Found
+      
+      
         res.writeHead(404, { 'Content-Type': 'text/html' });
         fs.readFile('./404.html', (err, content) => {
           res.end(content, 'utf-8');
         });
-      } else {
-        // Server Error
-        res.writeHead(500);
-        res.end('Sorry, there was a problem with the server.');
-      }
+      
     } else {
       res.writeHead(200, { 'Content-Type': contentType });
       res.end(content, 'utf-8');
@@ -36,7 +32,7 @@ const server = http.createServer((req, res) => {
   });
 });
 
-// Start the server
+
 server.listen(port, () => {
   console.log(`Server running at http://localhost:${port}/`);
 });
