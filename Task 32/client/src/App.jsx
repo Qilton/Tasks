610deca97917a53b744a1ccb1633d4e53d52e100
Qilton <swayambhalotia@gmail.com>
2024-09-02 18:04:29 +0530

    streaming

---
+++ Task 32/client/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <video className='video' controls src="http://localhost:3000/video"></video>
+    </>
+  )
+}
+
+export default App
---
+++ Task 32/server/server.js
@@ -0,0 +1,49 @@
+
+import express from 'express';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import { createReadStream, statSync } from 'fs';
+
+const app = express();
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res) => {
+  res.send('Hello, World!');
+});
+
+app.get('/video', (req, res) => {
+
+  const videoPath = path.join(__dirname, 'public', 'video.mp4');
+  const range= req.headers.range
+  const stat = statSync(videoPath);
+  const fileSize = stat.size;
+   if (range) {
+    const [start, end] = range.replace(/bytes=/, "").split("-").map(Number);
+    const chunkStart = start || 0;
+    const chunkEnd = Math.min(end || fileSize - 1, fileSize - 1);
+
+    res.writeHead(206, {
+      "Content-Range": `bytes ${chunkStart}-${chunkEnd}/${fileSize}`,
+      "Accept-Ranges": "bytes",
+      "Content-Length": chunkEnd - chunkStart + 1,
+      "Content-Type": "video/mp4",
+    });
+
+    const stream = createReadStream(videoPath, { start: chunkStart, end: chunkEnd });
+    stream.pipe(res);
+  } else {
+    res.sendFile(videoPath);
+  }
+  
+ 
+
+});
+
+const port = 3000;
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
